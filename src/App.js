162cd09db089edd6f218ccbf46fb89493c1649ea
Mahmood28 <mahmoodalwatani@gmail.com>
2021-01-20 16:22:48 +0300

    Done with Product List Task.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
+import products from "./products";
+import "./App.css";
+
 function App() {
+  const productsList = products.map((product) => (
+    <div className="books">
+      <img src={product.image} alt={product.name} height="300" />
+      <p>
+        {product.name} <br></br> {product.price} BD
+      </p>
+    </div>
+  ));
   return (
     <div>
       <center>
         <h1> Bed, Bookstore, and Beyond</h1>
         <h2> The Best Place to Nap and Read </h2>
         <img src="bookstore.jfif" />
+        <br></br>
+
+        {/* products  */}
+        <grid>{productsList}</grid>
       </center>
     </div>
   );
---
+++ src/products.js
@@ -0,0 +1,27 @@
+const products = [
+  {
+    name: "The Outer Limits of Reason",
+    price: 9.99,
+    image: "https://images.gr-assets.com/books/1381287623l/17841838.jpg",
+  },
+  {
+    name: "Alex Ferguson: My Autobiography",
+    price: 4.99,
+    image:
+      "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fimg1.imagesbn.com%2Fp%2F9780340919408_p0_v3_s260x420.jpg",
+  },
+  {
+    name: "The Girl On The Train",
+    price: 7.99,
+    image:
+      "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimages-na.ssl-images-amazon.com%2Fimages%2FI%2F51Gs9di1qWL._SY344_BO1%2C204%2C203%2C200_.jpg",
+  },
+  //   {
+  //     name: "A Promised Land",
+  //     price: 4.99,
+  //     image:
+  //       "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fmarlowbookshop.co.uk%2Fwp-content%2Fuploads%2F2020%2F09%2FA-Promised-Land.jpg",
+  //   },
+];
+
+export default products;
